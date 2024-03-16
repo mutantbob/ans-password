e5fc4ef09bf1180b3a3ede0ff95f56966b275a0c
Robert Forsman <git@thoth.purplefrog.com>
2024-03-16 14:19:27 -0400

    rework with instances

--- src/required_symbols.rs
+++ src/required_symbols.rs
@@ -14,48 +14,39 @@ impl<T: Clone + PartialEq> ClassWeight<T> {
         }
     }
 }
-/*
-struct Requirement<T: Clone + PartialEq> {
-    requirement: Vec<ClassWeight<T>>,
-}
 
-impl<T: Clone + PartialEq> Requirement<T> {
-    pub fn new(requirement: impl Into<Vec<ClassWeight<T>>>) -> Self {
-        Self {
-            requirement: requirement.into(),
-        }
-    }
-
-    pub(crate) fn adjusted_weights(&self, remaining: i32) -> Vec<ClassWeight<T>> {
-        Vec::from_iter(self.requirement.iter().cloned())
-    }
-}
-*/
 //
 
-pub struct Adjustmotron {}
+pub struct Adjustmotron {
+    required_prev: u32,
+    optional_prev: u32,
+}
+
 impl Adjustmotron {
-    pub fn restricted_weight(
-        remaining_symbols: u32,
-        required_prev: u32,
-        optional_prev: u32,
-    ) -> (u32, u32) {
-        if remaining_symbols <= 1 {
-            (required_prev, 0)
-        } else {
-            let fewer = remaining_symbols - 1;
-            let (r_w, o_w) = Self::weight(fewer, required_prev, optional_prev);
-            let w_a = r_w + o_w;
-            let (r_w, o_w) = Self::restricted_weight(fewer, required_prev, optional_prev);
-            let w_b = r_w + o_w;
-            (required_prev * w_a, optional_prev * w_b)
+    pub fn new(required_prev: u32, optional_prev: u32) -> Self {
+        Self {
+            required_prev,
+            optional_prev,
         }
     }
 
-    pub fn weight(remaining_symbols: u32, required_prev: u32, optional_prev: u32) -> (u32, u32) {
-        let sum = required_prev + optional_prev;
+    pub fn restricted_weight(&self, remaining_symbols: u32) -> (u32, u32) {
+        if remaining_symbols <= 1 {
+            (self.required_prev, 0)
+        } else {
+            let fewer = remaining_symbols - 1;
+            let (r_w, o_w) = self.weight(fewer);
+            let w_a = r_w + o_w;
+            let (r_w, o_w) = self.restricted_weight(fewer);
+            let w_b = r_w + o_w;
+            (self.required_prev * w_a, self.optional_prev * w_b)
+        }
+    }
+
+    pub fn weight(&self, remaining_symbols: u32) -> (u32, u32) {
+        let sum = self.required_prev + self.optional_prev;
         let pow = (1..remaining_symbols).into_iter().fold(1, |a, _b| a * sum);
-        (required_prev * pow, optional_prev * pow)
+        (self.required_prev * pow, self.optional_prev * pow)
     }
 }
 
@@ -72,38 +63,48 @@ mod test {
     use super::Adjustmotron;
 
     #[test]
-    pub fn test3() {
+    pub fn test1() {
+        let atron = Adjustmotron::new(3, 4);
         {
-            let (a, b) = Adjustmotron::weight(1, 3, 4);
+            let (a, b) = atron.weight(1);
             assert_eq!(3, a);
             assert_eq!(4, b);
         }
 
         {
-            let (a, b) = Adjustmotron::restricted_weight(1, 3, 4);
+            let (a, b) = atron.restricted_weight(1);
             assert_eq!(3, a);
             assert_eq!(0, b);
         }
     }
 
     #[test]
-    pub fn test4() {
+    pub fn test2() {
         {
-            let (a, b) = Adjustmotron::restricted_weight(2, 1, 1);
+            let (a, b) = Adjustmotron::new(1, 1).restricted_weight(2);
             assert_eq!(2, a);
             assert_eq!(1, b);
         }
 
         {
-            let (a, b) = Adjustmotron::restricted_weight(2, 2, 2);
+            let (a, b) = Adjustmotron::new(2, 2).restricted_weight(2);
             assert_eq!(2 * 4, a);
             assert_eq!(2 * 2, b);
         }
 
         {
-            let (a, b) = Adjustmotron::restricted_weight(2, 3, 4);
+            let (a, b) = Adjustmotron::new(3, 4).restricted_weight(2);
             assert_eq!(3 * (4 + 3), a);
             assert_eq!(4 * 3, b);
+        }
+    }
+
+    #[test]
+    pub fn test3() {
+        {
+            let (a, b) = Adjustmotron::new(1, 1).restricted_weight(8);
+            assert_eq!(128, a);
+            assert_eq!(127, b);
         }
     }
 }
